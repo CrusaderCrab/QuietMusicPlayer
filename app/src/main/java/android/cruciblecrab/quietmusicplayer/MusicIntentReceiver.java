3de3db2ff33d4f8c188795a9264605ad197f66d0
CrusaderCrab <cormac.keane@hotmail.com>
2016-03-06 16:44:20 +0000

    AUDIO_BECOMING_NOISY Intent now handled. Media pauses when headphones unplugged.

---
+++ app/src/main/java/android/cruciblecrab/quietmusicplayer/MusicIntentReceiver.java
@@ -0,0 +1,18 @@
+package android.cruciblecrab.quietmusicplayer;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by CrusaderCrab on 06/03/2016.
+ */
+public class MusicIntentReceiver extends android.content.BroadcastReceiver {
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        if (intent.getAction().equals(
+                android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
+            MediaLogic.LocalBinder binder = MediaLogicConnection.getBinder();
+            binder.pauseSong();
+        }
+    }
+}
